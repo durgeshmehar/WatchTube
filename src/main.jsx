c801b2adb9ef645864e64e2d912542497a7c5b93
Durgesh Mehar <108683573+durgeshmehar@users.noreply.github.com>
2023-08-23 15:11:04 +0530

    file import error resolved

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './App'
-import './Main.css'
+import './main.css'
 
 ReactDom.createRoot(document.getElementById('root')).render(<App />);
