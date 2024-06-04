48758866711fb88c6221dbd1235337dce3136e37
Durgesh Mehar <108683573+durgeshmehar@users.noreply.github.com>
2024-06-05 03:20:00 +0530

    Added Vidstream application information in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
 	<Stack direction="row" alignItems="center" p={2} sx={{zIndex:'20',position:'sticky' , background: '#000' , top:0 ,justifyContent:'space-between'}}>
 		<Link to='/' sx={{ display:'flex' ,alignItems :'center'}} >
-			<img src={youtubeIcon} alt="logo" height={45} />
+			<img src={youtubeIcon} alt="logo" height={45} /> <h3> Vidstream </h3>
 		</Link>
 		<SearchBar />
 	</Stack>
